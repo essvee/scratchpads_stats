3457ce96f8f79981f4ef5a5e9d07a44372e63a02
essvee <s.vincent@nhm.ac.uk>
2018-01-24 11:59:30 +0000

    Improves exception message and adds list comprehension

--- scratchpads_stats/src/scratchpads.py
+++ scratchpads_stats/src/scratchpads.py
@@ -3,24 +3,24 @@ import pymysql
 
 
 def connect():
+    # TODO - can/should global vars go somewhere else?
+    global databases
+
     # Get auth details + date
     with open('server-permissions.txt', 'r') as f:
         keys = f.read().splitlines()
         host, user, password, database = keys
 
-    sql = "SHOW DATABASES;"
-    databases = []
+    sql = "SHOW DATABASES"
 
     # Connect and get a list of available databases
     with pymysql.connect(host=host, user=user, password=password) as cursor:
 
             try:
                 cursor.execute(sql)
-                for n in cursor.fetchall():
-                    databases.append(n[0])
+                databases = [row[0] for row in cursor.fetchall()]
             except pymysql.Error as e:
-                print(sql)
-                print(e)
+                print(f"Error: {e} \nQuery: '{sql}'")
 
 
 if __name__ == '__main__':
