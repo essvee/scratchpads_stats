5fc3a0893d4bf6e577ca4a069b775daf250d0b5b
essvee <s.vincent@nhm.ac.uk>
2018-01-24 13:40:21 +0000

    Merges check_scratchpad_db() into connect()

--- scratchpads_stats/src/scratchpads.py
+++ scratchpads_stats/src/scratchpads.py
@@ -1,11 +1,7 @@
-from sshtunnel import SSHTunnelForwarder
 import pymysql
 
 
 def connect():
-    # TODO - can/should global vars go somewhere else?
-    global databases
-
     sql = "SHOW DATABASES"
 
     # Connect and get a list of available databases
@@ -13,47 +9,26 @@ def connect():
             try:
                 cursor.execute(sql)
                 databases = [row[0] for row in cursor.fetchall()]
-            except pymysql.Error as e:
-                print(f"Error: {e} \nQuery: '{sql}'")
 
-    check_scratchpad_db()
+                for db in databases:
+                    # Connect to database
+                    sql = f"USE {db}"
+                    cursor.execute(sql)
 
+                    # check it's a scratchpad and add to new list if so
+                    sql = "SHOW TABLES LIKE 'node_counter'"
+                    cursor.execute(sql)
 
-# Removes non-scratchpad dbs from list
-def check_scratchpad_db():
-    scratch_dbs = []
-
-    with pymysql.connect(host=host, user=user, password=password) as cursor:
-        for db in databases:
-            try:
-                # Connect to database
-                sql = f"USE {db}"
-                cursor.execute(sql)
-
-                # check it's a scratchpad - if not, remove from list
-                sql = "SHOW TABLES LIKE 'node_counter'"
-                cursor.execute(sql)
-
-                # Add scratchpads to scratch_dbs list
-                # TODO - is the else clause the best way to do 'ignore this one'?
-                scratch_dbs.append(db) if len(cursor.fetchall()) is not 0 else None
+                    # Add scratchpads to scratch_dbs list
+                    # TODO - is the else None clause the best way to do 'ignore this one'? else continue doesn't work...
+                    scratch_dbs.append(db) if len(cursor.fetchall()) is not 0 else None
 
             except pymysql.Error as e:
                 print(f"Error: {e} \nQuery: '{sql}'")
 
     print(scratch_dbs)
 
-    # try:
-    #     for i in cursor.fetchall():
-    #         s = len(i)
-    #         print(f"{db} returns result of length {s}")
-    #     # if cursor.fetchall() is None:
-    #     #     print(f"{db} is not a scratchpad")
-    #     # else:
-    #     #     print(f"{db} is a scratchpad!")
-    # except pymysql.Error as e:
-    #     print(f"Error: {e} \nQuery: '{sql}'")
-
+scratch_dbs = []
 
 # Get auth details + date
 with open('server-permissions.txt', 'r') as f:
